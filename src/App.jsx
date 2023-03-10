8d7eb44a3edf1060a6e95e8d9624cdcbf9d66b58
Ahnaf Ahamed <ahnafahamed4@gmail.com>
2023-03-10 22:35:57 +0530

    clean app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,17 @@
-import { Button, Center, Stack, Title } from "@mantine/core";
 import { Route, Routes, useNavigate } from "react-router-dom";
-import Expenses from "./components/Expenses";
-import Form from "./components/Form";
 import ProtectedRoute from "./utils/ProtectedRoute";
 import Profile from "./pages/Profile";
-import supabase from "./utils/SupabaseClient";
 import Auth from "./pages/Auth";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  const navigate = useNavigate();
   return (
     <ProtectedRoute>
         <Routes>
           <Route
             exact
             path="/"
-            element={
-              <Stack align="center">
-                <Title>Only Expense</Title>
-                <Form />
-                <Expenses />
-                <Button
-                  variant="light"
-                  color="red"
-                  uppercase
-                  onClick={async () => {
-                    const { error } = await supabase.auth.signOut();
-                    navigate("/auth");
-                  }}
-                >
-                  Log Out
-                </Button>
-              </Stack>
-            }
+            element={<Dashboard/>}
           />
           <Route path="/profile" element={<Profile />} />
           <Route path="/auth" element={<Auth />} />
