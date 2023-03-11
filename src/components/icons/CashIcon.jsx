2cad4d2dde773e49bdfb7d38956f21aceb39e017
Ahnaf Ahamed <ahnafahamed4@gmail.com>
2023-03-11 16:28:20 +0530

    add icons

---
+++ src/components/icons/CashIcon.jsx
@@ -0,0 +1,54 @@
+const CashIcon = () => {
+  return (
+    <svg
+      width="24"
+      height="25"
+      viewBox="0 0 24 25"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_18_61)">
+        <path
+          d="M9 12.2695C9 13.0652 9.31607 13.8282 9.87868 14.3909C10.4413 14.9535 11.2044 15.2695 12 15.2695C12.7956 15.2695 13.5587 14.9535 14.1213 14.3909C14.6839 13.8282 15 13.0652 15 12.2695C15 11.4739 14.6839 10.7108 14.1213 10.1482C13.5587 9.5856 12.7956 9.26953 12 9.26953C11.2044 9.26953 10.4413 9.5856 9.87868 10.1482C9.31607 10.7108 9 11.4739 9 12.2695Z"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M3 8.26953C3 7.7391 3.21071 7.23039 3.58579 6.85532C3.96086 6.48024 4.46957 6.26953 5 6.26953H19C19.5304 6.26953 20.0391 6.48024 20.4142 6.85532C20.7893 7.23039 21 7.7391 21 8.26953V16.2695C21 16.8 20.7893 17.3087 20.4142 17.6837C20.0391 18.0588 19.5304 18.2695 19 18.2695H5C4.46957 18.2695 3.96086 18.0588 3.58579 17.6837C3.21071 17.3087 3 16.8 3 16.2695V8.26953Z"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M18 12.2695H18.01"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M6 12.2695H6.01"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_18_61">
+          <rect
+            width="24"
+            height="24"
+            fill="white"
+            transform="translate(0 0.269531)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export default CashIcon;
